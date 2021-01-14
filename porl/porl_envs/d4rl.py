de9017d2835b1ff9436ec1373056ac99f6869da3
Servon <zewen.li@polixir.ai>
2021-01-14 18:43:05 +0800

    fix bugs

--- porl/porl_envs/d4rl.py
+++ porl/porl_envs/d4rl.py
@@ -7,5 +7,4 @@ def make_env(task):
     env_data = core.EnvData()
     env_data.set_name(task)
     env.get_dataset = env_data.get_dataset
-    env.get_dataset_by_traj_num = env_data.get_dataset_by_traj_num
     return env
