2dddb22c4d00ec6c328fa0badfbbe52ee5540ea3
Servon <zewen.li@polixir.ai>
2021-01-14 18:47:10 +0800

    fix bugs

--- porl/porl_envs/mujoco.py
+++ porl/porl_envs/mujoco.py
@@ -5,6 +5,7 @@ from porl import core
 def make_env(task):
     env = gym.make(task, exclude_current_positions_from_observation=False)
     env_data = core.EnvData()
-    env_data.set_name(task)
+    env.set_name = env_data.set_name
     env.get_dataset = env_data.get_dataset
+    env.set_reward_func = env_data.set_reward_func
     return env
