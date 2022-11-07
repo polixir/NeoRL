899d0000e0bf11a8aebbd66f9bbfc84322a4614c
qinrj <2428921608@qq.com>
2022-11-07 20:23:52 +0800

    add get_name for mujoco

--- neorl/neorl_envs/mujoco.py
+++ neorl/neorl_envs/mujoco.py
@@ -6,6 +6,7 @@ def make_env(task):
     env = gym.make(task, exclude_current_positions_from_observation=False)
     env_data = core.EnvData()
     env.set_name = env_data.set_name
+    env.get_name = env_data.get_name
     env.get_dataset = env_data.get_dataset
     env.set_reward_func = env_data.set_reward_func
     env.get_reward_func = env_data.get_reward_func
